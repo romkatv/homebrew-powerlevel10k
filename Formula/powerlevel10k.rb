2daa8e9d80710841ebcb325e33cd36d9fbfeb26b
romkatv <roman.perepelitsa@gmail.com>
2020-05-13 12:38:16 +0200

    upgrade to v1.9.0

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -1,12 +1,14 @@
 class Powerlevel10k < Formula
   desc "A Zsh Theme"
   homepage "https://github.com/romkatv/powerlevel10k"
-  url "https://github.com/romkatv/powerlevel10k/archive/v1.7.0.tar.gz"
-  sha256 "e4148d2ac921defb2039f0dfc127d56043f592fb3736787a057a18a15196a60e"
+  url "https://github.com/romkatv/powerlevel10k/archive/v1.9.0.tar.gz"
+  sha256 "e4d88e08b2d17c3ddcf1fa41da8e1992e1a425afdcb0b70849da9543c0a97b7b"
 
-  bottle :unneeded
+  depends_on "zsh"
 
   def install
+    system 'zsh', '-fc', 'GITSTATUS_CACHE_DIR="$PWD"/gitstatus/usrbin ./gitstatus/install -f'
+    system 'zsh', '-fc', 'emulate zsh -o no_aliases; for f in *.zsh-theme internal/*.zsh gitstatus/*.zsh gitstatus/install; do zcompile -R -- $f.zwc $f || exit; done'
     prefix.install Dir["*"]
   end
 
@@ -19,7 +21,7 @@ class Powerlevel10k < Formula
   end
 
   test do
-    assert_match "OK",
-      shell_output("zsh -dfic '. #{opt_prefix}/powerlevel10k.zsh-theme && (( ${+P9K_SSH} )) && echo OK'")
+    assert_match "SUCCESS",
+      shell_output("zsh -fic '. #{opt_prefix}/powerlevel10k.zsh-theme && (( ${+P9K_SSH} )) && echo SUCCESS'")
   end
 end
