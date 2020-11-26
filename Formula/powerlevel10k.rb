4e547854687610327353e5ff4b30a1677bdc7f1e
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-11-26 07:41:04 +0100

    upgrade to v1.14.1

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -1,14 +1,13 @@
 class Powerlevel10k < Formula
   desc "A Zsh Theme"
   homepage "https://github.com/romkatv/powerlevel10k"
-  url "https://github.com/romkatv/powerlevel10k/archive/v1.13.0.tar.gz"
-  sha256 "84e23f801523e8d4084f6c9a726d918433f6d6c8b91b1540e1f6ecbed2c40a73"
+  url "https://github.com/romkatv/powerlevel10k/archive/v1.14.0.tar.gz"
+  sha256 "7c00cb49cfb928d013461b148f0ad09c623e334c37deabc86a7ef79896489f43"
 
   depends_on "zsh"
 
   def install
-    system 'zsh', '-fc', 'unset -m "GITSTATUS_*"; GITSTATUS_CACHE_DIR="$PWD"/gitstatus/usrbin ./gitstatus/install -f'
-    system 'zsh', '-fc', 'emulate zsh -o no_aliases; for f in *.zsh-theme internal/*.zsh gitstatus/*.zsh gitstatus/install; do zcompile -R -- $f.zwc $f || exit; done'
+    system 'make', 'pkg'
     prefix.install Dir["*"]
   end
 
@@ -17,6 +16,8 @@ class Powerlevel10k < Formula
       To activate powerlevel10k, add the following line to .zshrc:
 
         source #{opt_prefix}/powerlevel10k.zsh-theme
+
+      If your .zshrc sets ZSH_THEME, remove that line.
     EOS
   end
 
