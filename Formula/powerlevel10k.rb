edc2bd1d22500b160dd70e1ec51cf354364bce57
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-11-26 08:03:24 +0100

    upgrade to v1.14.1

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -1,8 +1,8 @@
 class Powerlevel10k < Formula
   desc "A Zsh Theme"
   homepage "https://github.com/romkatv/powerlevel10k"
-  url "https://github.com/romkatv/powerlevel10k/archive/v1.14.0.tar.gz"
-  sha256 "7c00cb49cfb928d013461b148f0ad09c623e334c37deabc86a7ef79896489f43"
+  url "https://github.com/romkatv/powerlevel10k/archive/v1.14.1.tar.gz"
+  sha256 "cb45299308c7dc34f5fcb8a9a1f1622fb0df23062bf8e03253a55d117266faad"
 
   depends_on "zsh"
 
