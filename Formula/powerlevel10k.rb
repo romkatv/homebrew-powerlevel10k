1ffc464e9db36ae6a991e02710fb7c7d8b0dc60e
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2022-01-27 20:57:03 +0100

    remove a dependency on zsh

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -4,8 +4,6 @@ class Powerlevel10k < Formula
   url "https://github.com/romkatv/powerlevel10k/archive/v1.16.0.tar.gz"
   sha256 "354bb17a033abf0800f69dc0d95c5ce3cfc8072c38c722d91ec26178a4f5ebc5"
 
-  depends_on "zsh"
-
   def install
     system 'make', 'pkg'
     prefix.install Dir["*"]
